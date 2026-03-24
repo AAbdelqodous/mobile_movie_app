f1d6c5b94f38a2a2995b0fd07f56ced64745e39a
Ahmed.Elsayed <ahmed.elsayed@manpower.gov.kw>
2026-03-24 10:13:22 +0300

    Home tab

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,20 @@
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
-import { Image, ScrollView, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
+import MovieCard from "../components/MovieCard";
 import SearchBar from "../components/SearchBar";
 
 export default function Index() {
   const router = useRouter();
-  
+  const {
+    data : movies, 
+    loading : moviesLoading, 
+    error : moviesError
+  } = useFetch( () => fetchMovies({ query: '' }) );
+
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0"/>
@@ -14,11 +22,44 @@ export default function Index() {
       <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} 
         contentContainerStyle={{ minHeight: '100%', paddingBottom: 10 }}>
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
-        <View className="flex-1 mt-5">
-          <SearchBar 
-            onPress={() => router.push('/search')}
-            placeholder="Search for movies, TV shows, actors..." />
-        </View>
+
+        {moviesLoading? (
+          <ActivityIndicator size="large" color="#0000ff" className="mt-10 self-center"/>
+        ) : moviesError? (
+          <Text>Error: {moviesError?.message}</Text>
+          
+        ) : (
+          <View className="flex-1 mt-5">
+            <SearchBar 
+              onPress={() => router.push('/search')}
+              placeholder="Search for movies, TV shows, actors..." />
+
+            <>
+              <Text className="text-lg text-white font-bold mt-5 mb-3">Trending Movies</Text>
+
+              <FlatList
+                data={movies}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => (
+                    <MovieCard  {...item }/>
+                )}
+                scrollEnabled={false}
+                numColumns={3}
+                columnWrapperStyle={{ 
+                  justifyContent: 'flex-start',
+                  gap: 20,
+                  paddingRight: 5,
+                  marginBottom: 10 
+                }}
+                className="mt-2 pb-32"
+                scrollEnabled={false}
+              />
+
+            </>
+          </View>
+        )}
+
+        
       </ScrollView>
     </View>
   );
--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,10 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname)
 
+config.watchFolders = [
+  // ...config.watchFolders
+]
+
+config.blacklistRE = /.*(AppData\\Roaming\\Claude|AppData\\Roaming\\jupyter).*/
+
 module.exports = withNativeWind(config, { input: './app/global.css' })
